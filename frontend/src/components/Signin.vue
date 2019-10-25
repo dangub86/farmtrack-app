bf6663cea4a4ec9373d0fa9864e06eb779abb88f
danie <gubbiottidaniele@gmail.com>
2019-10-25 18:51:33 +0200

    Updated frontend

---
+++ frontend/src/components/Signin.vue
@@ -0,0 +1,287 @@
+<template>
+<body class="bg">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 offset-md-3 text-center ">
+        <form class="form" action method="">     
+          <div class="form-login">
+            <br>
+            <h4>SIGN IN</h4>
+            <br>
+            <input
+              name="name"
+              type=" text"
+              v-model="service.name"
+              class="form-control input-sm"
+              placeholder="name"
+              required
+            >
+             <input
+              name="surname"
+              type=" text"
+              v-model="service.surname"
+              class="form-control input-sm"
+              placeholder="surname"
+              required
+            >
+
+            <input
+              name="email"
+              type=" text"
+              v-model="service.email"
+              class="form-control input-sm"
+              placeholder="email"
+              required
+            >
+            
+            <input
+              name="password"
+              type="password"
+              v-model="service.password"
+              @keydown.enter.prevent="addCategory"
+              class="form-control input-sm chat-input"
+              placeholder="password"
+              required
+            >
+           <br>
+            <br>
+            <br>
+            <div class="wrapper">
+              <span class="group-btn">
+                <button type="login" href class="btn btn-primary btn-md" @mouseenter="validator()" @click="startchecker()">
+                  Sign in
+                  <i class="fa fa-sign-in"></i>
+                </button>
+              </span>
+            </div>
+          </div>
+        </form>
+      </div>
+
+
+
+    </div>
+  </div>
+  <br>
+  <br>
+  <br>
+  <br>
+</body>
+</template>
+
+
+<script>
+// import axios from 'axios'
+import { AXIOS } from "./http-common";
+
+export default {
+  name: "service",
+
+  data() {
+    return {
+      msg: "HowTo call REST-Services:",
+      checking:null,
+      params:null,
+      accesso:null,
+      answer:null,
+      response: [],
+      errors: [],
+       service: {
+          name: '',
+          surname: '',
+          email: '',
+          password: ''
+    },
+        showResponse: false,
+        retrievedUser: {},
+        showRetrievedUser: false
+  }
+},
+methods: {
+      // Fetches posts when the component is created.
+      startchecker(){
+        
+      }
+}
+}
+
+</script>
+
+<style scoped>
+
+
+input { 
+	width: 100%; 
+	margin-bottom: 10px; 
+	background: rgba(0,0,0,0.3);
+	border: none;
+	outline: none;
+	padding: 10px;
+	font-size: 13px;
+	color: #fff;
+	text-shadow: 1px 1px 1px rgba(0,0,0,0.3);
+	border: 1px solid rgba(0,0,0,0.3);
+	border-radius: 4px;
+	box-shadow: inset 0 -5px 45px rgba(100,100,100,0.2), 0 1px 1px rgba(255,255,255,0.2);
+	-webkit-transition: box-shadow .5s ease;
+	-moz-transition: box-shadow .5s ease;
+	-o-transition: box-shadow .5s ease;
+	-ms-transition: box-shadow .5s ease;
+	transition: box-shadow .5s ease;
+}
+@media (max-width: 640px) {
+  body {
+    font-size: 1.3rem;
+  }
+  h1 {
+    font-size: 16px;
+  }
+  h5 {
+    font-size: 12;
+  }
+}
+@media (max-width: 767px) {
+  .desc {
+    margin-top: 50px;
+  }
+  h5 {
+    font-size: 8px;
+  }
+  .desc {
+    width: 250px;
+    min-height: 0px !important;
+    margin-top: 50px;
+  }
+  .form-login {
+    width: 250px;
+  }
+  .container {
+    margin-right: 10px;
+    padding: 50px !important;
+  }
+}
+
+@media (min-width: 641px) {
+  body {
+    font-size: 1.2rem;
+  }
+
+
+  h5 {
+    font-size: 14px;
+  }
+}
+
+@media (min-width: 960px) {
+  body {
+    font-size: 1.4rem;
+  }
+   h5 {
+    font-size: 20px;
+  }
+}
+
+@media (min-width: 1100px) {
+  body {
+    font-size: 1.6rem;
+  }
+  h5 {
+    font-size: 20px;
+  }
+  .desc {
+    margin-left: 40px;
+  }
+}
+
+h5.test {
+  word-wrap: break-word;
+}
+body {
+  height: 100%;
+}
+
+.uploading-image {
+  display: flex;
+}
+.resized {
+  width: auto;
+  height: 150px;
+}
+.bg {
+  background: lightgrey;
+  max-height: 100vh !important;
+  overflow: hidden !important;
+}
+
+.bar {
+    height: 10vh;
+    background: limegreen;
+}
+
+.container {
+  padding: 110px;
+}
+::placeholder {
+  /* Chrome, Firefox, Opera, Safari 10.1+ */
+  color: #ffffff69 !important;
+  opacity: 1; /* Firefox */
+  font-size: 18px !important;
+  font-weight: 200 !important;
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+}
+.form-login {
+  background-color: rgba(0, 0, 0, 0.55);
+  padding-top: 10px;
+  padding-bottom: 20px;
+  padding-left: 20px;
+  padding-right: 20px;
+  border-radius: 15px;
+  border-color: #d2d2d2;
+  border-width: 5px;
+  color: white;
+  box-shadow: 0 1px 0 #cfcfcf;
+}
+.form-control {
+  background: transparent !important;
+  color: white !important;
+  font-size: 18px !important;
+}
+h1 {
+  color: white !important;
+}
+h5 {
+  border: 0 solid #fff;
+  border-bottom-width: 1px;
+  padding-bottom: 10px;
+  text-align: center;
+}
+
+.form-control {
+  border-radius: 10px;
+}
+.text-white {
+  color: white !important;
+}
+
+.wrapper {
+  text-align: center;
+}
+
+.a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+.desc {
+  background-color: rgba(0, 0, 0, 0.55);
+  padding-top: 10px;
+  padding-bottom: 20px;
+  padding-left: 20px;
+  padding-right: 20px;
+  border-radius: 15px;
+  border-color: #d2d2d2;
+  border-width: 5px;
+  color: white;
+  box-shadow: 0 1px 0 #cfcfcf;
+  min-height: 450px;
+}
+</style>
