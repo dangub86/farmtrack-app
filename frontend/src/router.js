c4f051a92f37d99e10245291d20d887daaf7c778
danie <gubbiottidaniele@gmail.com>
2019-11-20 18:47:14 +0100

    Added TreeDetails component (fix treeDetails in router)

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -5,7 +5,6 @@ import Service from '@/components/Service'
 import Land from '@/components/AddLand'
 import Tree from '@/components/AddTree'
 import Trees from '@/components/Trees'
-import TreeDetails from '@/components/TreeDetails'
 
 Vue.use(Router)
 
@@ -36,11 +35,6 @@ export default new Router({
       name: 'Trees',
       component: Trees
     },
-    {
-      path: '/treeDetails',
-      name: 'TreeDetails',
-      component: TreeDetails
-    }
 
   ]
 
